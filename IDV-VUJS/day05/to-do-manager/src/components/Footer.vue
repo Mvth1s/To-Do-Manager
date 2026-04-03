298476a05118358f9fca4f3090f893426d250bd9
Mvth1s <aguadomathis@gmail.com>
2026-04-03 16:33:32 +0200

    idv_vujs: ajout du components footer

---
+++ IDV-VUJS/day05/to-do-manager/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<script lang="ts"></script>
+
+<template>
+<footer class="footer"><p>&copy; 2026 Code2Work - Aider les étudiants à rester productifs</p></footer>
+</template>
+
+<style>
+.footer{
+  background: #f5f5f5;
+  text-decoration: center;
+  padding: 2rem;
+  margin-top: 3rem;
+  border-top: 1px solid #ddd;
+  color: #666;
+}
+p {
+  margin: 0;
+}
+</style>
