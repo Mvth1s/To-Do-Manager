56dbd9926ce72248be508ac4eff27c7b3803a09a
Mvth1s <aguadomathis@gmail.com>
2026-04-03 16:39:09 +0200

    idv_vujs: création du components TaskItem.vue vide pour le moment

---
+++ IDV-VUJS/day05/to-do-manager/src/components/TaskItem.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="task-item">
+    <h2>Tes Tâches</h2>
+  </div>
+</template>
+
+<style>
+.task-item {
+  padding: 1rem;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  margin-bottom: 1rem;
+}
+</style>
