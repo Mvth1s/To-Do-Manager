41b88f5b5c7f5a680e2f565542c685d4f93c8046
Mvth1s <aguadomathis@gmail.com>
2026-04-03 16:52:45 +0200

    idv_vujs: exo14, App.vue: ajout de toute les variables nécessaire pour le moment

--- IDV-VUJS/day05/to-do-manager/src/App.vue
+++ IDV-VUJS/day05/to-do-manager/src/App.vue
@@ -1,6 +1,15 @@
 <script setup lang="ts">
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
+
+import { ref } from 'vue'
+import type { Task } from '@/types/Task'
+import { Status } from '@/types/Task'
+
+const task = ref<Task[]>([])
+const newTaskTitle = ref<string>('')
+const errorMessage = ref<string>('')
+const nextId = ref<number>(1)
 </script>
 
 <template>
@@ -10,14 +19,14 @@ import Footer from '@/components/Footer.vue'
 </template>
 
 <style>
-#app{
+#app {
   display: flex;
   flex-direction: column;
   min-height: 100vh;
 }
 
-main{
-  flex:1;
+main {
+  flex: 1;
   max-width: 1000px;
   margin: 2rem auto;
   width: 100%;
