9e1621a039c8c64f7e8119c48da58a9232c3a440
Mvth1s <aguadomathis@gmail.com>
2026-04-03 16:36:31 +0200

    idv_vujs: création du components TaskForm.vue vide pour le moment

---
+++ IDV-VUJS/day05/to-do-manager/src/components/TaskForm.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="task-form">
+    <h2>Créer une nouvelle tâche</h2>
+  </div>
+</template>
+
+<style>
+.task-form {
+  background: white;
+  padding: 2rem;
+  border-radius: 8px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+}
+</style>
