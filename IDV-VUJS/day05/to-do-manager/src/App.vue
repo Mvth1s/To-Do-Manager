c9668828c47d76e86c1bf78cbae564222a781963
Mvth1s <aguadomathis@gmail.com>
2026-04-03 16:42:27 +0200

    idv_vujs: exo14, App.vue: import des components header et footer

--- IDV-VUJS/day05/to-do-manager/src/App.vue
+++ IDV-VUJS/day05/to-do-manager/src/App.vue
@@ -1,11 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <Header />
+  <main class="container"></main>
+  <Footer />
 </template>
 
-<style scoped></style>
+<style>
+#app{
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+main{
+  flex:1;
+  max-width: 1000px;
+  margin: 2rem auto;
+  width: 100%;
+  padding: 0 1rem;
+}
+</style>
