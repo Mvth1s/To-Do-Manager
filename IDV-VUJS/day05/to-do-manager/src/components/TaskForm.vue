391cc465a78c7f5ccb184aec2926d60a015de2e9
Mvth1s <aguadomathis@gmail.com>
2026-04-04 20:37:49 +0200

    idv_vujs: ajout du formulaire des taches

--- IDV-VUJS/day05/to-do-manager/src/components/TaskForm.vue
+++ IDV-VUJS/day05/to-do-manager/src/components/TaskForm.vue
@@ -3,6 +3,16 @@
 <template>
   <div class="task-form">
     <h2>Créer une nouvelle tâche</h2>
+    
+    <form @submit.prevent="createTask">
+      <div class="form-group">
+        <label for="title">Titre de la tâche</label>
+        <input id="title" v-model="nextaskTitle" type="text" placeholder="Entre ta tâche..." class="input"/>
+      </div>
+      <div v-if="errorMessage" class="error-message">{{errorMessage}}</div>
+      
+      <button type="submit" class="btn btn-primary">Ajouter une Tâche</button>
+    </form>
   </div>
 </template>
 
